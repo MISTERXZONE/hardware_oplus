d7687edf965b6c75be0cb3d20f909d4f6274d4ee
pjgowtham <pjgowtham@gmail.com>
2024-11-22 14:13:04 +0530

    hidl: fingerprint: Set dim layer zOrder
    Requires: I367b2c395aeb8cb7cb9e3cc38eca98477befe007
    Change-Id: I881db78186fa9c7b350e79ac4ef90f53ee4cfbcf

--- hidl/fingerprint/UdfpsExtension.cpp
+++ hidl/fingerprint/UdfpsExtension.cpp
@@ -33,3 +33,7 @@ uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
 uint64_t getUdfpsUsageBits(uint64_t usageBits, bool /*touched*/) {
     return usageBits;
 }
+
+uint32_t getUdfpsDimZOrder(uint32_t z) {
+    return z;
+}
